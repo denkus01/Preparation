30f6312aecad106faa0be7fb420bf76f3a5fdfc5
denkus01 <Transas159951>
2019-10-03 23:07:39 +0300

    Merge Sort Two Arrays

---
+++ src/java/Arrays/MergeTwoSortedLists.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode handler = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                handler.next = l1;
+                l1 = l1.next;
+            } else {
+                handler.next = l2;
+                l2 = l2.next;
+            }
+            handler = handler.next;
+        }
+        if (l1 != null) {
+            handler.next = l1;
+        } else if (l2 != null) {
+            handler.next = l2;
+        }
+        return head.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
