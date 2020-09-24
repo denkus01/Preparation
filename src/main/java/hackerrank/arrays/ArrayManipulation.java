270633bde77300b1d2cf413d1120ddd331d4797a
Denis Kus <viluyr@yandex.ru>
2020-09-24 13:44:40 +0300

    Hackerrank Arrays preparation kit

---
+++ src/main/java/hackerrank/arrays/ArrayManipulation.java
@@ -0,0 +1,52 @@
+package hackerrank.arrays;
+
+import java.util.Arrays;
+
+/**
+ * Starting with a 1-indexed array of zeros and a list of operations, for each operation add a value to each of the array
+ * element between two given indices, inclusive. Once all operations have been performed, return the maximum value in
+ * your array.
+ * For example, the length of your array of zeros n = 10. Your list of queries is as follows:
+ * a b k
+ * 1 5 3
+ * 4 8 7
+ * 6 9 1
+ * <p>
+ * Add the values of between the indices and inclusive:
+ * index->
+ * 1  2  3  4  5  6  7  8  9  10
+ * [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+ * [3, 3, 3, 3, 3, 0, 0, 0, 0, 0]
+ * [3, 3, 3,10,10, 7, 7, 7, 0, 0]
+ * [3, 3, 3,10,10, 8, 8, 8, 1, 0]
+ */
+
+public class ArrayManipulation {
+
+    static long arrayManipulation(int n, int[][] queries) {
+        long[] array = new long[n + 1];
+        for (int[] query : queries) {
+            int a = query[0], b = query[1], k = query[2];
+            array[a] += k;
+            if (b + 1 <= n) {
+                array[b + 1] -= k;
+            }
+        }
+        long x = 0, max = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            x += array[i];
+            if (max < x) max = x;
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+        int[][] arr = {{1, 5, 3}, {4, 8, 7}, {6, 9, 1}};
+
+        int result = (int) arrayManipulation(10, arr);
+
+        System.out.print("Array: " + Arrays.deepToString(arr));
+        System.out.println("result: " + result);
+    }
+}
--- src/main/java/hackerrank/arrays/HourGlassSumTask.java
+++ src/main/java/hackerrank/arrays/HourGlassSumTask.java
@@ -1,22 +1,21 @@
 package hackerrank.arrays;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HourGlassSumTask {
 
     static int hourglassSum(int[][] arr) {
         int sum = 0;
-        for(int i=0;i<=3; i++){
-            for(int j=0; j<=3; j++){
-                int arrSumm = arr[i][j] + arr[i][j+1] + arr[i][j+2]
-                        + arr[i+1][j+1] + arr[i+2][j]
-                        + arr[i+2][j+1] + arr[i+2][j+2];
-                if (i==0 && j==0){
+        for (int i = 0; i <= 3; i++) {
+            for (int j = 0; j <= 3; j++) {
+                int arrSumm = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                        + arr[i + 1][j + 1] + arr[i + 2][j]
+                        + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                if (i == 0 && j == 0) {
                     sum = arrSumm;
-                } else if (arrSumm >sum){
+                } else if (arrSumm > sum) {
                     sum = arrSumm;
                 }
             }
@@ -26,28 +25,21 @@ public class HourGlassSumTask {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
+    public static void main(String[] args) {
+        int max = 100;
+        int min = 0;
         int[][] arr = new int[6][6];
 
         for (int i = 0; i < 6; i++) {
-            String[] arrRowItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
             for (int j = 0; j < 6; j++) {
-                int arrItem = Integer.parseInt(arrRowItems[j]);
-                arr[i][j] = arrItem;
+                arr[i][j] = (int) ((Math.random() * ++max) + min);
+                ;
             }
         }
 
         int result = hourglassSum(arr);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
+        System.out.print("Array: " + Arrays.deepToString(arr));
+        System.out.println("result: " + result);
     }
 }
